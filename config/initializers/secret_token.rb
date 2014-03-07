a90becedc30964fae01712af5d14e9787c452716
Justus Daniel Eapen Jr <justuseapen@gmail.com>
2014-03-07 17:50:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDoChallenge::Application.config.secret_key_base = 'c56a9498f56fe23dece4d1deb4940ac7292db093d565e11647769c79cb50d5975ee9ea8cbc5903591967eaaaab9dfe14737d32c7dd0f3b482e29dae2b3d9d97d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ToDoChallenge::Application.config.session_store :cookie_store, key: '_to_do_challenge_session'
